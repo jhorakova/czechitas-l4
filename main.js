bc2f8d8782619f3ae57d62a6a014b320c2ae136c
Jana Horáková <horakovajana2017@gamil.com>
2023-02-15 12:11:22 +0100

    prubeyne ulozeni

--- main.js
+++ main.js
@@ -1,5 +1,82 @@
-// tady je místo pro náš program
+// Homework
 
+let nadpis = document.querySelector('h1')
+nadpis.innerHTML = 'Homework 4'
+
+/*V konzoli prohlížeče si založte proměnnou title a uložte do ní název svého oblíbeného filmu (např. Pán prstenů). Proveďte následující úkoly.
+
+Vypište do konzole počet znaků názvu.
+Vypište název filmu převedený na velká písmena.
+Vyřízněte z názvu prvních pět písmen.
+Vyřízněte z názvu posledních pět písmen.*/
+
+//Oblibeny film
+let title = 'Vykoupení z věznice Shawshank'
+console.log(title)
+
+//Pocet znaku konzole
+console.log(title.length)
+
+//Prevod na velka pismena - zobrazeno web
+let capsLock = document.querySelector('.capsLock')
+capsLock.innerHTML = 'Oblíbený film velkými písmeny: ' + title.toUpperCase()
+
+//Prvnich pet pismen - zobrazeno web
+let vyriznutiPrvni = document.querySelector('.vyriznutiPrvni')
+vyriznutiPrvni.innerHTML = 'Prvních pět písmen: ' + title.slice(0,5)
+
+//Poslednich pet pismen - zobrazeno web
+let vyriznutiPosledni = document.querySelector('.vyriznutiPosledni')
+vyriznutiPosledni.innerHTML = 'Posleních pět písmen: ' + title.slice(24,29)
+
+
+/*Vytvořte stránku, která bude pracovat s e-mailovými adresami ve formátu
+
+jmeno.prijmeni@domena
+Tedy například:
+
+petr.novak@gmail.com
+romana.nejedla@czechitas.cz
+slavomir.ponuchalek@yahoo.com
+Postupujte dle následujících kroků.
+
+Vytvořte jednoduchou webovou stránku s JavaScriptovým programem.
+Nechte uživatele zadat jeho e-mail a uložte si jej do proměnné email.
+Pomocí metody indexOf najděte v tomto e-mailu pozici znaku zavináč. Tuto pozici si uložte do proměnné atIndex.
+Pomocí metody slice vyřízněte z e-mailu první část představující jméno a příjmení uživatele.
+Dále z e-mailu vyřízněte název domény tedy například gmail.com.
+Ze získaných informací vytvořte objekt, který bude vypadat například takto:
+const parsedEmail = {
+	userName: 'slavomir.ponuchalek',
+	domain: 'yahoo.com',
+}
+Pro kontrolu vypište tento objekt do stránky. Každou hodnotu vypište jako odstavec.*/
+
+let email = prompt('Zadej svůj email: ')
+
+let zadanyEmail = document.querySelector('.zadanyEmail')
+zadanyEmail.innerHTML = 'Tvůj oblíbený email je: ' + email
+
+let zavinac = document.querySelector('.zavinac')
+zavinac.innerHTML = 'Zavináč najdeš na ' + email.indexOf('@') + '.' + ' ' + 'pozici'
+
+let vyriznutiEmail = document.querySelector('.vyriznutiEmail')
+vyriznutiEmail.innerHTML = '' + email.slice(0, email.indexOf('@'))
+
+let domena = document.querySelector('.domena')
+domena.innerHTML = email.slice(email.indexOf('@')+1, email.length)
+
+
+let parsedEmail = {
+	userName: email.slice(0, email.indexOf('@')),
+	domain: email.slice(email.indexOf('@')+1, email.length)
+}
+//console.log(parsedEmail)
+
+//let {userName,domain} = parsedEmail
+
+//let vypisObjekt = document.querySelector('.vypisObjekt')
+//vypisObjekt.innerHTML = userName + domain
 
 
 
